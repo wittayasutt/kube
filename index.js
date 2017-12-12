1e7ba198a5086f01c5a5504495f6b6fe4ce71f56
wittayasutt <wittayasutt@gmail.com>
2017-12-12 16:38:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+	res.status(200).json('welcome to root')
+})
+
+app.get('/foo', (req, res) => {
+	res.status(200).json('welcome to foo')
+})
+
+app.get('/bar', (req, res) => {
+	res.status(200).json('welcome to bar')
+})
+
+app.listen(port, () => console.log('Magic happens on port', port))
+
+module.exports.app = app
